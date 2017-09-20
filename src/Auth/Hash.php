8f53130a639446b5ad2edb71086bf1408f7c01d5
Marat Nuriev <nurieff@gmail.com>
2017-09-21 00:49:46 +0500

    remove old

--- src/Auth/Hash.php
+++ src/Auth/Hash.php
@@ -5,19 +5,9 @@ namespace Spirit\Auth;
 class Hash extends \Spirit\Func\Hash
 {
 
-    public static function app($alias, $id)
-    {
-        return static::h([$alias, $id]);
-    }
-
     public static function password($password)
     {
         return password_hash($password, PASSWORD_DEFAULT);
-    }
-
-    public static function activation($user_id)
-    {
-        return password_hash($user_id, PASSWORD_DEFAULT);
     }
 
     public static function recovery($user_id)
